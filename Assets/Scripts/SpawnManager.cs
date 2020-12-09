0ff71db5f89df8f34b507840da21776cd1b0a553
cvr-software <cvrgaming@gmail.com>
2020-12-09 14:24:48 -0800

    Lesson 4.2 Complete

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+
+    private float spawnRange = 9.0f;
+    
+    void Start()
+    {
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosY = Random.Range(-spawnRange, spawnRange);
+        return new Vector3(spawnPosX, 0.0f, spawnPosY);
+    }
+}
