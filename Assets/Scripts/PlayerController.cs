f5e7edff0d4f7399ed9c110482e6c63e93a456ad
cvr-software <cvrgaming@gmail.com>
2020-12-09 14:14:12 -0800

    Lesson 4.1 Complete

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    private Rigidbody rb;
+    private GameObject focalPoint;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        rb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+}
