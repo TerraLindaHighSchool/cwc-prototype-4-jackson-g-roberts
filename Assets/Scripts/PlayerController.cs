7c953f97f3f1e4df306820f7d47de463ef271498
cvr-software <cvrgaming@gmail.com>
2020-12-09 14:38:43 -0800

    Lesson 4.3 Complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
 
+    public bool hasPowerup;
+
+    public GameObject powerupIndicator;
+
     private Rigidbody rb;
     private GameObject focalPoint;
+
+    private float powerupStrength = 15.0f;
     
     void Start()
     {
@@ -19,5 +25,37 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         rb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        powerupIndicator.transform.position = transform.position + new Vector3(0.0f, -0.5f, 0.0f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            powerupIndicator.gameObject.SetActive(true);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(true);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+            
+            Debug.Log("Player collided with " + collision.gameObject + " with powerup set to " + hasPowerup);
+
+            enemyRb.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        }
     }
 }
